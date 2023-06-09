23b2d6eda3dbfb0a1612256c6fdbedb6d8088aa9
saqWWS <132142425+saqWWS@users.noreply.github.com>
2023-06-09 23:36:04 +0400

    Update HomeWork-Caesar.py

--- HomeWork-Caesar/HomeWork-Caesar.py
+++ HomeWork-Caesar/HomeWork-Caesar.py
@@ -6,20 +6,43 @@ def alphabet():
     return alphabet
 
 
-def decode():
-    key = int(input("num:\t"))
-    text = input("encode:\t").lower()
+def encode(key, text):
     str_ = ""
     alpha = alphabet()
     for i in range(len(text)):
+        print(i)
         index_of_alpha = alpha.index(text[i])
-        print("Տառի ինդեքս", index_of_alpha)
-        print("Այն ինդեքսը, որը ստացվել է", index_of_alpha + key)
+        print("Տառի ինդեքս:", index_of_alpha)
+        print("Այն ինդեքսը, որը ստացվել է:", index_of_alpha + key)
         if index_of_alpha + key > len(alpha):
             print(alpha[len(alpha) - index_of_alpha - 1])
         else:
             str_ += alpha[index_of_alpha + key]
         print(str_)
+    return str_
 
 
-print(decode())
+_key = int(input("Please write the number:\t"))
+_text = input("You can only write text:\t").lower()
+print(encode(_key, _text))
+
+
+def decode(key, text):
+    str_ = ""
+    alpha = alphabet()
+    for i in range(len(text)):
+        print(i)
+        index_of_alpha = alpha.index(text[i])
+        print("Տառի ինդեքս:", index_of_alpha)
+        print("Այն ինդեքսը, որը ստացվել է:", index_of_alpha - key)
+        if index_of_alpha + key > len(alpha):
+            print(alpha[len(alpha) - index_of_alpha - 1])
+        else:
+            str_ += alpha[index_of_alpha - key]
+        print(str_)
+    return str_
+
+
+_key = int(input("Please write the number:\t"))
+_text = input("You can only write text:\t").lower()
+print(decode(_key, _text))
